4a16a23fdb064943b50af4341f30f0c44795291f
Rafael Lima <rafa.olv.lima@gmail.com>
2018-07-06 23:18:07 -0300

    Server class (Still needs to be tested)

--- Classes/Server.py
+++ Classes/Server.py
@@ -15,7 +15,8 @@ class Server:
         self.__max_hops = max_hops
         self.clients = []
 
-    # public methods
+    # public methods:
+
     '''
         The add_client method returns:
         0 -> a client was successfully added
@@ -33,31 +34,59 @@ class Server:
         return return_value
 
     '''
-        The update_clients_hop returns:
-        0 -> if all the clients hop were successfully updated
-        1 -> if the clients list is empty
+        The update_server_status must be called each time a clock tick passes:
+        It returns:
+        0 -> All occured as expected
+        1 -> No client removed
+        2 -> Client list was empty
     '''
 
-    def update_clients_hop(self):
+    def update_server_status(self):
+        return_value = self._update_clients_hop()
+        return_value = return_value + self._remove_client_from_server_based_on_hop()
+        return return_value
+
+    # protected methods:
+
+    '''
+            The update_clients_hop returns:
+            0 -> if all the clients hop were successfully updated
+            1 -> if the clients list is empty
+        '''
+
+    def _update_clients_hop(self):
         return_value = 1
         new_clients_list = []
-        if len(self.clients) > 0:
+        if len(self.clients) > 0 and self.__no_connected_clients > 0:
             for c in self.clients:
                 c.increase_hop()
                 new_clients_list.append(c)
             return_value = 0
         return return_value
 
-    # protected methods:
+    def _remove_client_from_server_using_hash_code(self, hash_code):
+        if self.__no_connected_clients > 0:
+            for c in self.clients:
+                if c.get_hash_code() == hash_code:
+                    assert isinstance(c, Client)
+                    self.clients.remove(c)
+                    self.__no_connected_clients = self.__no_connected_clients - 1
+
+    '''
+        The _remove_client_from_server_based_on_hop method returns:
+        1 -> if no client was removed
+        0 -> There was (were) client (s) removed. 
+    '''
+
     def _remove_client_from_server_based_on_hop(self):
         clients_hash_to_remove = []
+        return_value = 1
         if len(self.clients) > 0:
             for i in range(0, len(self.clients)):
                 if self.clients[i].get_current_hop() >= self.__max_hops:
                     clients_hash_to_remove.append(self.clients[i].get_hash_code())
+                    return_value = 0
         if len(clients_hash_to_remove) > 0:
-            for hash in clients_hash_to_remove:
-                for c in self.clients:
-                    if c.get_hash_code() == hash:
-                        assert isinstance(c, Client)
-                        self.clients.remove(c)
+            for hash in clients_hash_to_remove:  # type: hash: string that contains clients hashcode
+                self._remove_client_from_server_using_hash_code(hash)
+        return return_value
