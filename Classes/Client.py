18f7917f454789e5774ab73cd603e1850a5e4508
Rafael Lima <rafa.olv.lima@gmail.com>
2018-07-06 21:40:57 -0300

    Classes package and Client.py (still needs to be tested)

---
+++ Classes/Client.py
@@ -0,0 +1,29 @@
+##########################################
+# Author: Rafael Lima
+# Date of Creation: Friday, July 6 2018
+##########################################
+
+
+from uuid import uuid4
+from time import strftime
+
+class Client:
+
+    # constructor:
+    def __init__(self):
+        self.__hash_code = self._generate_hash_code()
+        self.__hops = 0
+        self.__date_of_creation = strftime("%Y-%m-%d--%H-%M-%S")
+
+    # public methods:
+    def increase_hop(self):
+        self.__hops = self.__hops + 1
+
+    def get_hash_code(self):
+        return self.__hash_code
+
+    # protected and static methods:
+    @staticmethod
+    def _generate_hash_code(self):
+        code = str(uuid4().hex)
+        return code
